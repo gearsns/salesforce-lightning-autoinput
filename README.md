445af09124f03a5be4654d871847a47b2781fa92
gears <gears.ns@gmail.com>
2021-11-30 13:27:06 +0900

    0.0.2

--- README.md
+++ README.md
@@ -1,17 +1,91 @@
 # salesforce-lightning-autoinput
-Using javascript to autofill fields in salesforce lightning
+Javascriptによる salesforce lightning の自動データ登録ライブラリです。
 
 ## Features
-
+対応している入力種類は以下の通り
+* lightning-input
+	* text
+	* checkbox
+* lightning-textarea
+* lightning-datepicker
+* lightning-timepicker
+* lightning-datetimepicker
+* lightning-input-address
+* lightning-combobox
+* lightning-grouped-combobox
+* lightning-button
 ## Usage
+※sectionは省略可
 
-```
-run(label_name, value)
-```
+* lightning-input
+	* text
+	```
+	slai.run(label_name, "value", section)
+	```
+	* checkbox
+	```
+	slai.run(label_name, 'check', section)
+	slai.run(label_name, '', section)
+	```
+	※value : 'check'でチェックボックスにチェックが入ります。<br>
+	それ以外の値を設定した場合は、チェックを外します。
+* lightning-textarea
+	```
+	slai.run(label_name, "value\r\nvalue", section)
+	```
+* lightning-datepicker
+	```
+	slai.run(label_name, "yyyy/mm/dd", section)
+	```
+	※書式は、salesforceの設定にしたがって指定してください。
+* lightning-timepicker
+	```
+	slai.run(label_name, "hh:mm", section)
+	```
+	※書式は、salesforceの設定にしたがって指定してください。
+* lightning-datetimepicker
+	```
+	slai.run(label_name, "yyyy/mm/dd hh:mm", section)
+	slai.run(label_name, date:"yyyy/mm/dd", time: "hh:mm"}, section)
+	※書式は、salesforceの設定にしたがって指定してください。
+	```
+* lightning-input-address
+	```
+	slai.run("address", {province: "province", city: "city", street: "street", postalCode: "postal-code", country: "country"})
+	```
+	※書式は、salesforceの設定にしたがって指定してください。
+* lightning-combobox
+	```
+	slai.run(label_name, "value", section)
+	```
+* lightning-grouped-combobox
+	```
+	slai.run(label_name, "value", section)
+	```
+* lightning-button
+	```
+	slai.run({ type: "button", wait: true }, "value", section)
+	```
+	※type : "button" 固定<br>
+	wait: 保存時、待機する場合は trueを指定
 
 ## Example
 ```
-run("SLA Expiration Date", "2000. 1. 2")
+await import('https://********.visualforce.com/salesforce-lightning-autoinput.js').then(async res => {
+	const slai = new res.SalesforceLightningAutoinput();
+	await slai.run("address", {province: "province", city: "city", street: "street", postalCode: "postal-code", country: "country"})
+	await slai.run("SLA Expiration Date", "2021/01/01 01:23")
+	await slai.run("SLA Expiration Date", {date:"2022/02/04", time: "01:23"})
+	await slai.run({ type: "button", wait: true }, "Save");
+});
+
 ```
 
 ![](image/example.png)
+
+## Notes
+* そのままライブラリをimportしようとすると「content-security-policy」エラーが出るので<br>
+WevView2を使用したプログラムで用いる場合は、ソースをローカルに保存して仮想ホストで 「visualforce.com」のドメイン扱いにするなどの対応をしてください。
+```
+ WebView2.CoreWebView2.SetVirtualHostNameToFolderMapping("SalesforceLightningAutoinput.visualforce.com", "contents", Microsoft.Web.WebView2.Core.CoreWebView2HostResourceAccessKind.Allow);
+```
--- salesforce-lightning-autoinput.js
+++ salesforce-lightning-autoinput.js
@@ -1,234 +1,332 @@
-function _sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms))
-}
+export class SalesforceLightningAutoinput {
+	constructor() {
 
-async function FindAllDescendants(node, func) {
-	async function f(n) {
-		var nodes = n.childNodes;
-		if (nodes) {
-			for (let item of [...nodes]) {
-				node = await func(item);
-				if (node) {
-					return node;
+	}
+
+	_sleep = ms => {
+		return new Promise((resolve) => setTimeout(resolve, ms))
+	}
+
+	FindAllDescendants = async (node, func) => {
+		const f = async n => {
+			if (n) {
+				let ret = null;
+				if (n.shadowRoot) {
+					ret = await f(n.shadowRoot);
+					if (ret) {
+						return ret;
+					}
 				}
-				node = await f(item);
-				if (node) {
-					return node;
+				var nodes = n.childNodes;
+				if (nodes) {
+					for (let item of [...nodes]) {
+						ret = await func(item);
+						if (ret) {
+							return ret;
+						}
+						ret = await f(item);
+						if (ret) {
+							return ret;
+						}
+					}
 				}
 			}
-			if (n.shadowRoot) {
-				return await f(n.shadowRoot);
-			}
-		}
-		return null;
-	}
-	return await f(node);
-}
-
-async function getActionBodyElement() {
-	for (let i = 0; i < 50; i++) {
-		let e = document.querySelector(".actionBody");
-		if (e) {
-			return e;
-		}
-		await _sleep(500);
-	}
-	return null;
-}
-
-async function getLabel(node, label_name) {
-	return await FindAllDescendants(node, async c_node => {
-		if (c_node && c_node.className && c_node.className.match && c_node.className.match(/(^| +)slds\-form\-element__label($| +)/)
-			&& (c_node.innerText === `*${label_name}` || c_node.innerText === label_name)
-		) {
-			return c_node;
-		}
-		return null;
-	})
-}
-
-async function inputValue(el_actionbody, label_name, input_value) {
-	return await FindAllDescendants(el_actionbody, async node => {
-		if (!node || !node.tagName || !node.tagName.match) {
 			return null;
 		}
-		if (node.tagName.match(/lightning\-textarea/i)) {
-			if (!await getLabel(node, label_name)) {
+		return await f(node);
+	}
+
+	getActionBodyElement = async () => {
+		for (let i = 0; i < 50; i++) {
+			let e = document.querySelector(".actionBody");
+			if (e) {
+				return e;
+			}
+			await this._sleep(500);
+		}
+		return null;
+	}
+
+	getLabel = async (node, label_name) => {
+		return await this.FindAllDescendants(node, async c_node => {
+			if (c_node && c_node.className && c_node.className.match && c_node.className.match(/(^| +)slds\-form\-element__label($| +)/)
+				&& (c_node.innerText === `*${label_name}` || c_node.innerText === label_name)
+			) {
+				return c_node;
+			}
+			return null;
+		})
+	}
+
+
+	listLabel = async el_actionbody => {
+		let cur_section_name = "";
+		return await this.FindAllDescendants(el_actionbody, async node => {
+			if (!node || !node.tagName || !node.tagName.match) {
 				return null;
 			}
-			let el_input = await FindAllDescendants(node, async c_node => {
-				if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/textarea/i)) {
-					return c_node;
-				}
+			if (node.className && node.className.match && node.className.match(/(^| +)section\-header\-title($| +)/)) {
+				cur_section_name = node.innerText;
+				console.log(`${cur_section_name}`);
 				return null;
-			});
-			if (el_input) {
-				el_input.value = input_value;
-				el_input.click();
-				el_input.dispatchEvent(new Event("blur"));
-				return node;
+			} else if (node && node.className && node.className.match && node.className.match(/(^| +)slds\-form\-element__label($| +)/)) {
+				console.log(`+ ${node.innerText}`);
 			}
-		} else if (node.tagName.match(/lightning\-combobox/i)) {
-			if (!await getLabel(node, label_name)) {
-				return null;
-			}
-			let el_input = await FindAllDescendants(node, async c_node => {
-				if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/input/i)) {
-					return c_node;
-				}
-				return null;
-			});
-			if (el_input && el_input.className === "slds-input slds-combobox__input") {
-				for (let i = 0; i < 50; i++) {
-					el_input.click();
-					let el_item = await FindAllDescendants(node, async c_node => {
-						if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/lightning\-base\-combobox\-item/i)
-							&& c_node.getAttribute("data-value") === input_value) {
-							return c_node;
-						}
-						return null;
-					});
-					if (el_item) {
-						el_item.click();
-						el_input.dispatchEvent(new Event("blur"));
-						return node;
-					}
-					await _sleep(500);
-				}
-			}
-		} else if (node.tagName.match(/lightning\-grouped\-combobox/i)) {
-			if (!await getLabel(node, label_name)) {
+			return null;
+		})
+	}
+
+	inputValue = async (el_actionbody, label_name, input_value, section_name) => {
+		let cur_section_name = "";
+		return await this.FindAllDescendants(el_actionbody, async node => {
+			if (!node || !node.tagName || !node.tagName.match) {
 				return null;
 			}
-			let el_input = await FindAllDescendants(node, async c_node => {
-				if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/input/i)) {
-					return c_node;
+			if (node.className && node.className.match && node.className.match(/(^| +)section\-header\-title($| +)/)) {
+				if (node.innerText) {
+					cur_section_name = node.innerText;
 				}
 				return null;
-			});
-			if (el_input && el_input.className === "slds-input slds-combobox__input") {
-				el_input.value = input_value;
-				el_input.dispatchEvent(new Event("input"));
-				el_input.dispatchEvent(new Event("change"));
-				el_input.blur();
-				node.dispatchEvent(new CustomEvent("textinput", { "detail": { "text": input_value } }));
-				for (let i = 0; i < 50; i++) {
-					el_input.dispatchEvent(new KeyboardEvent("keydown", { key: "a" }));
-					el_input.click();
-					let cc_node = await FindAllDescendants(node, async cc_node => {
-						if (cc_node && cc_node.className && cc_node.className.match
-							&& cc_node.className.match(/(^| +)slds\-listbox__option\-(text_entity|meta_entity)($| +)/)
-							&& cc_node.innerText && cc_node.innerText === input_value) {
-							return cc_node;
-						}
-						return null;
-					})
-					if (cc_node) {
-						cc_node.focus();
-						cc_node.click();
-						node.dispatchEvent(new Event("blur"));
-						return node;
-					}
-					await _sleep(500);
-				}
-			}
-		} else if (node.tagName.match(/lightning\-input/i)) {
-			if (!await getLabel(node, label_name)) {
+			} else if (section_name && section_name !== cur_section_name) {
 				return null;
-			}
-			let elDateTimePicker = await FindAllDescendants(node, async c_node => {
-				if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/lightning\-datetimepicker/i)) {
-					return c_node;
+			} else if (node.tagName.match(/lightning\-textarea/i)) {
+				if (!await this.getLabel(node, label_name)) {
+					return null;
 				}
-				return null;
-			});
-			if (elDateTimePicker) {
-				let value_date = input_value;
-				let value_time = "";
-				if (input_value.match(/(.*) (.*)/)) {
-					value_date = RegExp.$1;
-					value_time = RegExp.$2;
-				}
-				await FindAllDescendants(elDateTimePicker, async c_node => {
-					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/(lightning\-datepicker|lightning\-timepicker)/i)) {
-						if (c_node.tagName.match(/datepicker/i)) {
-							await FindAllDescendants(c_node, async cc_node => {
-								if (cc_node && cc_node.tagName && cc_node.tagName.match && cc_node.tagName.match(/input/i)) {
-									cc_node.value = value_date;
-									cc_node.dispatchEvent(new Event("input"));
-									cc_node.dispatchEvent(new Event("change"));
-									cc_node.dispatchEvent(new Event("blur"));
-								}
-								return null;
-							})
-						} else {
-							await FindAllDescendants(c_node, async cc_node => {
-								if (cc_node && cc_node.tagName && cc_node.tagName.match && cc_node.tagName.match(/input/i)) {
-									cc_node.value = value_time;
-									cc_node.dispatchEvent(new Event("input"));
-									cc_node.dispatchEvent(new Event("change"));
-									cc_node.dispatchEvent(new Event("blur"));
-								}
-								return null;
-							})
-						}
+				let el_input = await this.FindAllDescendants(node, async c_node => {
+					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/textarea/i)) {
+						return c_node;
 					}
 					return null;
-				})
-				return node;
-			}
-			let elDateOrTimePicker = await FindAllDescendants(node, async c_node => {
-				if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/(lightning\-datepicker|lightning\-timepicker)/i)) {
-					return await FindAllDescendants(c_node, async cc_node => {
-						if (cc_node && cc_node.tagName && cc_node.tagName.match && cc_node.tagName.match(/input/i)) {
-							cc_node.value = input_value;
-							cc_node.dispatchEvent(new Event("input"));
-							cc_node.dispatchEvent(new Event("change"));
-							cc_node.dispatchEvent(new Event("blur"));
-							return cc_node;
-						}
-						return null;
-					})
-				}
-				return null;
-			});
-			if (elDateOrTimePicker) {
-				return node;
-			}
-			let el_input = await FindAllDescendants(node, async c_node => {
-				if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/input/i)) {
-					return c_node;
-				}
-				return null;
-			});
-			if (el_input) {
-				if (el_input.type === "checkbox") {
-					if (input_value === "check") {
-						el_input.cheked = true;
-					} else {
-						el_input.cheked = false;
-					}
-				} else {
+				});
+				if (el_input) {
 					el_input.value = input_value;
 					el_input.click();
 					el_input.dispatchEvent(new Event("blur"));
+					return node;
+				}
+			} else if (node.tagName.match(/lightning\-combobox/i)) {
+				if (!await this.getLabel(node, label_name)) {
+					return null;
+				}
+				let el_input = await this.FindAllDescendants(node, async c_node => {
+					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/input/i)) {
+						return c_node;
+					}
+					return null;
+				});
+				if (el_input && el_input.className === "slds-input slds-combobox__input") {
+					for (let i = 0; i < 50; i++) {
+						el_input.click();
+						let el_item = await this.FindAllDescendants(node, async c_node => {
+							if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/lightning\-base\-combobox\-item/i)
+								&& (c_node.getAttribute("data-value") === input_value
+									|| c_node.innerText === input_value)
+							) {
+								return c_node;
+							}
+							return null;
+						});
+						if (el_item) {
+							el_item.click();
+							el_input.dispatchEvent(new Event("blur"));
+							return node;
+						}
+						await this._sleep(500);
+					}
+				}
+			} else if (node.tagName.match(/lightning\-grouped\-combobox/i)) {
+				if (!await this.getLabel(node, label_name)) {
+					return null;
+				}
+				let el_input = await this.FindAllDescendants(node, async c_node => {
+					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/input/i)) {
+						return c_node;
+					}
+					return null;
+				});
+				if (el_input && el_input.className === "slds-input slds-combobox__input") {
+					el_input.value = input_value;
+					el_input.dispatchEvent(new Event("input"));
+					el_input.dispatchEvent(new Event("change"));
+					el_input.blur();
+					node.dispatchEvent(new CustomEvent("textinput", { "detail": { "text": input_value } }));
+					for (let i = 0; i < 50; i++) {
+						el_input.dispatchEvent(new KeyboardEvent("keydown", { key: "a" }));
+						el_input.click();
+						let cc_node = await this.FindAllDescendants(node, async cc_node => {
+							if (cc_node && cc_node.className && cc_node.className.match
+								&& cc_node.className.match(/(^| +)slds\-listbox__option\-(text_entity|meta_entity)($| +)/)
+								&& cc_node.innerText && cc_node.innerText === input_value) {
+								return cc_node;
+							}
+							return null;
+						})
+						if (cc_node) {
+							cc_node.focus();
+							cc_node.click();
+							node.dispatchEvent(new Event("blur"));
+							return node;
+						}
+						await this._sleep(500);
+					}
+				}
+			} else if (node.tagName.match(/lightning\-input\-address/i)) {
+				if (!await this.getLabel(node, label_name)) {
+					return null;
+				}
+				await this.FindAllDescendants(node, async c_node => {
+					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/(lightning\-input|lightning\-textarea)/i)) {
+						let attr = c_node.getAttribute("data-field");
+						if (input_value[attr]) {
+							c_node.value = input_value[attr];
+							c_node.dispatchEvent(new Event("input"));
+							c_node.dispatchEvent(new Event("blur"));
+						}
+						return null;
+					}
+				});
+				return node;
+			} else if (node.tagName.match(/lightning\-input/i)) {
+				if (!await this.getLabel(node, label_name)) {
+					return null;
+				}
+				let elDateTimePicker = await this.FindAllDescendants(node, async c_node => {
+					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/lightning\-datetimepicker/i)) {
+						return c_node;
+					}
+					return null;
+				});
+				if (elDateTimePicker) {
+					let value_date = input_value;
+					let value_time = "";
+					if (input_value.date) {
+						value_date = input_value.date;
+					}
+					if (input_value.time) {
+						value_time = input_value.time;
+					}
+					if (!input_value.date && !input_value.time && input_value.match(/(.*) (.*)/)) {
+						value_date = RegExp.$1;
+						value_time = RegExp.$2;
+					}
+					await this.FindAllDescendants(elDateTimePicker, async c_node => {
+						if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/(lightning\-datepicker|lightning\-timepicker)/i)) {
+							if (c_node.tagName.match(/datepicker/i)) {
+								await this.FindAllDescendants(c_node, async cc_node => {
+									if (cc_node && cc_node.tagName && cc_node.tagName.match && cc_node.tagName.match(/input/i)) {
+										cc_node.value = value_date;
+										cc_node.dispatchEvent(new Event("input"));
+										cc_node.dispatchEvent(new Event("change"));
+										cc_node.dispatchEvent(new Event("blur"));
+									}
+									return null;
+								})
+							} else {
+								await this.FindAllDescendants(c_node, async cc_node => {
+									if (cc_node && cc_node.tagName && cc_node.tagName.match && cc_node.tagName.match(/input/i)) {
+										cc_node.value = value_time;
+										cc_node.dispatchEvent(new Event("input"));
+										cc_node.dispatchEvent(new Event("change"));
+										cc_node.dispatchEvent(new Event("blur"));
+									}
+									return null;
+								})
+							}
+						}
+						return null;
+					})
+					return node;
+				}
+				let elDateOrTimePicker = await this.FindAllDescendants(node, async c_node => {
+					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/(lightning\-datepicker|lightning\-timepicker)/i)) {
+						return await this.FindAllDescendants(c_node, async cc_node => {
+							if (cc_node && cc_node.tagName && cc_node.tagName.match && cc_node.tagName.match(/input/i)) {
+								cc_node.value = input_value;
+								cc_node.dispatchEvent(new Event("input"));
+								cc_node.dispatchEvent(new Event("change"));
+								cc_node.dispatchEvent(new Event("blur"));
+								return cc_node;
+							}
+							return null;
+						})
+					}
+					return null;
+				});
+				if (elDateOrTimePicker) {
+					return node;
+				}
+				let el_input = await this.FindAllDescendants(node, async c_node => {
+					if (c_node && c_node.tagName && c_node.tagName.match && c_node.tagName.match(/input/i)) {
+						return c_node;
+					}
+					return null;
+				});
+				if (el_input) {
+					if (el_input.type === "checkbox") {
+						if (input_value === "check") {
+							el_input.cheked = true;
+						} else {
+							el_input.cheked = false;
+						}
+					} else {
+						el_input.value = input_value;
+						el_input.click();
+						el_input.dispatchEvent(new Event("blur"));
+					}
+					return node;
+				}
+			}
+			return null;
+		});
+	}
+
+	buttonClick = async (el_actionbody, name, wait) => {
+		return await this.FindAllDescendants(el_actionbody, async node => {
+			if (!node || !node.tagName || !node.tagName.match) {
+				return null;
+			}
+			if (node.tagName.match(/lightning\-button/i) && node.innerText === name) {
+				node.click();
+				if (wait) {
+					await this._sleep(1000);
+					for (let i = 0; i < 50; ++i) {
+						if (!await this.FindAllDescendants(el_actionbody, async c_node => {
+							if (!c_node || !c_node.tagName || !c_node.tagName.match) {
+								return null;
+							}
+							if (c_node.tagName.match(/lightning\-spinner/i)) {
+								return c_node;
+							}
+						})) {
+							break;
+						}
+						await this._sleep(500);
+					}
+					await this._sleep(1000);
 				}
 				return node;
 			}
-		}
-		return null;
-	});
-}
+			return null;
+		})
+	}
 
-async function run(key, value) {
-	for (let i = 0; i < 50; ++i) {
-		let el_actionbody = await getActionBodyElement();
-		if (el_actionbody) {
-			if (await inputValue(el_actionbody, key, value)) {
-				break;
+	run = async (key, value, section) => {
+		if (key.type === "button") {
+			let el_actionbody = await this.getActionBodyElement();
+			if (el_actionbody) {
+				await this.buttonClick(el_actionbody, value, key.wait);
+			}
+		} else {
+			for (let i = 0; i < 50; ++i) {
+				let el_actionbody = await this.getActionBodyElement();
+				if (el_actionbody) {
+					if (await this.inputValue(el_actionbody, key, value, section)) {
+						break;
+					}
+				}
+				await this._sleep(500);
 			}
 		}
-		await _sleep(500);
 	}
 }
-
